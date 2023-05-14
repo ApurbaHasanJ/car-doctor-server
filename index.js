d231b2b75d661b9d860d221d83a181e17e3bca33
unknown <apurbahasanj@gmail.com>
2023-05-15 01:17:17 +0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
-console.log(process.env.DB_PASS);
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@believer.igrxpib.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,32 +18,136 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const verifyJWT = (req, res, next) => {
+  console.log("hit verify");
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ error: true, message: "Unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+    const orderCheckOutCollection = client.db("carDoctor").collection("orders");
+
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
+
+    // services Routes
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // Order CheckOut get
+    app.get("/checkouts", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log("came back after verify", decoded);
+
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ error: 1, message: "Forbidden access" });
+      }
+
+      // console.log(req.headers.authorization);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      // for all data
+      const result = await orderCheckOutCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // post
+    app.post("/checkouts", async (req, res) => {
+      const checkouts = req.body;
+      console.log(checkouts);
+      const result = await orderCheckOutCollection.insertOne(checkouts);
+      res.send(result);
+    });
+
+    // update order product
+    app.patch("/checkouts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedOrder = req.body;
+      console.log(updatedOrder);
+
+      const updateDoc = {
+        $set: {
+          status: updatedOrder.status,
+        },
+      };
+      const result = await orderCheckOutCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // delete order product
+    app.delete("/checkouts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await orderCheckOutCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Car Doctor is running");
+});
 
-
-
-app.get('/', (req, res) => {
-    res.send('Car Doctor is running')
-})
-
-
-
-app.listen(port, ()=>{
-    console.log(`car doctor is listening on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`car doctor is listening on ${port}`);
+});
